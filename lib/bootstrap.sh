7b48f9849aafdb8c304f4261525d9b49a618e317
Philip Stark <git@codechaos.ch>
2012-08-09 18:02:42 +0200

    update bootstrap.sh

--- lib/bootstrap.sh
+++ lib/bootstrap.sh
@@ -1,17 +1,31 @@
 #!/bin/bash
 
-cd ~
-
-# install oh-my-zsh
-echo "Cloning Oh My Zsh"
 hash git >/dev/null || {
     echo "Error: git not installed";
-    exit;
+    exit 2;
 }
 
-/usr/bin/env git clone https://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh || {
-    echo "Oh My Zsh is already installed";
-}
+cd ~
+
+# check for oh-my-zsh
+if [ ! -a ~/.oh-my-zsh ]; then
+    # install oh-my-zsh
+    echo "Cloning Oh My Zsh";
+    /usr/bin/env git clone https://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
+else
+    echo "Oh My Zsh is already installed.";
+fi
+
+
+
+# check for oh-my-zsh
+if [ ! -a ~/.dotfiles ]; then
+    echo "Cloning hellerbarde/dotfiles repository";
+    git clone https://github.com/hellerbarde/dotfiles.git ~/.dotfiles
+else
+    echo "~/.dotfiles exists already."
+    # TODO Maybe check if it's my own .dotfiles and if so, update it.
+fi
 
 if [ -f ~/.zshrc ] || [ -h ~/.zshrc ]
 then
@@ -20,12 +34,15 @@ then
     rm ~/.zshrc;
 fi
 
-echo "Cloning hellerbarde/dotfiles repository";
-git clone https://github.com/hellerbarde/dotfiles.git ~/.dotfiles
-
 echo "Linking stuff"
-echo ".zshrc"
+echo "for zsh"
 ln -s .dotfiles/zshrc ~/.zshrc;
-
-
+echo "for vim"
+ln -s .dotfiles/vim/vimrc.after ~/.vimrc.after
+ln -s .dotfiles/vim/vimrc.before ~/.vimrc.before
+ln -s .dotfiles/vim/gvimrc.after ~/.gvimrc.after
+ln -s .dotfiles/vim/gvimrc.before ~/.gvimrc.before
 echo "Change your shell to zsh: chsh -s `which zsh`"
+
+# TODO: Vim/Janus installation
+
